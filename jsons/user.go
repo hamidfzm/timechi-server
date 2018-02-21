094a9072188c3ce2dd33b8fcd5ce6610936485c9
Hamid FzM <hamidfzm@gmail.com>
2018-02-21 06:51:45 +0330

    Implement basic dummy user model with JSONs

---
+++ jsons/user.go
@@ -0,0 +1,28 @@
+package jsons
+
+import (
+	"github.com/hamidfzm/timechi-server/models"
+	"github.com/hamidfzm/timechi-server/helpers"
+)
+
+type RegisterV1 struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (j *RegisterV1) To(m *models.User) {
+	m.Name = j.Name
+	m.Email = j.Email
+	m.Password = helpers.HashPassword(j.Password)
+}
+
+type PublicProfileV1 struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func (j *PublicProfileV1) From(m *models.User) {
+	j.Name = m.Name
+	j.Email = m.Email
+}
