18f4cc8043e744acbd16440d4a8bb196459ad51a
Hamid FzM <hamidfzm@gmail.com>
2018-02-21 07:39:44 +0330

    Set viper default value and init cobra

--- helpers/config.go
+++ helpers/config.go
@@ -1,16 +1,46 @@
 package helpers
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+	"github.com/spf13/cobra"
+	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog"
+	"os"
+)
 
 type config struct {
 	Port      int
 	PrettyLog bool `mapstructure:"pretty_log"`
 }
 
-var Config *config
+var Config config
+var cfgFile string
 
 func init() {
-	viper.Set("port", 8080)
-	viper.Set("pretty_log", true)
+	cobra.OnInitialize(initConfig)
+	viper.SetDefault("port", 8080)
+	viper.SetDefault("pretty_log", true)
+}
+
+func initConfig() {
+	viper.SetConfigName("config")
+	
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		
+		// Search config in home directory with name ".cobra" (without extension).
+		viper.AddConfigPath("/etc/timechi/")
+		viper.AddConfigPath(".")
+	}
+	
+	if err := viper.ReadInConfig(); err != nil {
+		log.Warn().Msgf("Can't read config: %s", err)
+	}
 	viper.Unmarshal(&Config)
+	
+	if Config.PrettyLog {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+	}
 }
