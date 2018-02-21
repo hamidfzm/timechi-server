a66882f26f6afc1e7ff9dad8c3c70f276f634e7d
Hamid FzM <hamidfzm@gmail.com>
2018-02-21 07:53:02 +0330

    Implement server graceful shutdown

--- cmd/web.go
+++ cmd/web.go
@@ -8,6 +8,10 @@ import (
 	"fmt"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
+	"os"
+	"os/signal"
+	"context"
+	"time"
 )
 
 func init() {
@@ -20,16 +24,29 @@ var webCmd = &cobra.Command{
 	Use:   "web",
 	Short: "Start web server",
 	Run: func(cmd *cobra.Command, args []string) {
+		stop := make(chan os.Signal, 1)
+		signal.Notify(stop, os.Interrupt)
+		
 		addr := fmt.Sprintf(":%d", helpers.Config.Port)
-		log.Info().Msgf("Listening to %s", addr)
-		if err := http.ListenAndServe(
-			addr,
-			buildChain(
-				router.Router,
-				helpers.LoggerMiddleware,
-			)); err != nil {
-			log.Fatal().Msgf("Listening failed: %s", err)
-		}
+		
+		log.Info().Msgf("Listening on %s", addr)
+		
+		server := &http.Server{Addr: addr, Handler: buildChain(
+			router.Router,
+			helpers.LoggerMiddleware,
+		)}
+		
+		go func() {
+			if err := server.ListenAndServe(); err != nil {
+				log.Fatal().Msgf("%s", err)
+			}
+		}()
+		<-stop
+		
+		log.Info().Msg("Shutting down the server...")
+		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+		server.Shutdown(ctx)
+		log.Info().Msg("Server gracefully stopped")
 	},
 }
 
