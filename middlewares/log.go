b8bb8934cf96edf251eb1dee2bfe2687e2baab0c
Hamid FzM <hamidfzm@gmail.com>
2018-10-02 19:12:48 +0330

    Implement log middleware

--- middlewares/log.go
+++ middlewares/log.go
@@ -3,11 +3,32 @@ package middlewares
 import (
 	"net/http"
 	"github.com/rs/zerolog/log"
+	"time"
 )
+
+type statusRecorder struct {
+	http.ResponseWriter
+	status int
+}
+
+func (rec *statusRecorder) WriteHeader(code int) {
+	rec.status = code
+	rec.ResponseWriter.WriteHeader(code)
+}
 
 func LoggerMiddleware(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Info().Str("method", r.Method).Str("path", r.URL.Path).Msg("Request")
-		handler.ServeHTTP(w, r)
+		rec := statusRecorder{w, 200}
+		
+		start := time.Now()
+		handler.ServeHTTP(&rec, r)
+		end := time.Now()
+		
+		log.Info().
+			Str("method", r.Method).
+			Str("path", r.URL.Path).
+			Int("code", rec.status).
+			TimeDiff("duration", end, start).
+			Msg("Request")
 	})
 }
